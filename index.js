323c361a2607876177fe975bb431012a34bc84d9
pietrostraneo <straneo_p@yahoo.it>
2024-05-22 14:58:41 +0200

    Refactor media classes and instances, add toString method
    Refactor the code to use ES6 class syntax for Movie and Show. Add toString method to provide formatted media information when called. Use map to create movie and show instances based on mediaArray. This improves code readability and reusability.

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const mediaArray = [
     { title: 'Friends', year: 1994, genre: 'Comedy', rating: 8.9, type: 'tv', seasons: 10 }
 ];
 
-class movie {
-    title
-    year
-    genre
-    rating
-    type
+class Movie {
     constructor(title, year, genre, rating, type) {
         this.title = title;
         this.year = year;
@@ -18,16 +13,31 @@ class movie {
         this.rating = rating;
         this.type = type;
     }
-}
 
-class show extends movie {
-    seasons
-    constructor(title, year, genre, rating, type, seasons) {
-        this.title = title;
-        this.year = year;
-        this.genre = genre;
-        this.rating = rating;
-        this.type = type;
-        this.seasons = seasons;
+    toString() {
+        return `${this.title} è un film di genere ${this.genre}. È stato rilasciato nel ${this.year} ed ha un voto di ${this.rating}.`;
     }
 }
+
+class Show extends Movie {
+    constructor(title, year, genre, rating, type, seasons) {
+        super(title, year, genre, rating, type);
+        this.seasons = seasons;
+    }
+
+    toString() {
+        return `${this.title} è una serie tv di genere ${this.genre}. La prima stagione è stata rilasciata nel ${this.year} ed in totale sono state prodotte ${this.seasons} stagioni. Ha un voto di ${this.rating}.`;
+    }
+}
+
+const mediaInstances = mediaArray.map(media => {
+    if (media.type === 'movie') {
+        return new Movie(media.title, media.year, media.genre, media.rating, media.type);
+    } else if (media.type === 'tv') {
+        return new Show(media.title, media.year, media.genre, media.rating, media.type, media.seasons);
+    }
+});
+
+mediaInstances.forEach(mediaInstance => {
+    console.log(mediaInstance.toString());
+});
